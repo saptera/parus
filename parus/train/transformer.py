73a80243bd3a2ffaa9735355cb90923e89287e75
xl5liu <eiddeuil@hotmail.com>
2023-07-13 23:27:44 +0000

    trainable transformer

---
+++ parus/train/transformer.py
@@ -0,0 +1,143 @@
+import math
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from parus.train.datagen import get_train_datagen, get_val_datagen, get_test_datagen
+from parus.data import sim_sig_read, sim_lbl_read
+
+class SimulatedNoiseDataset(Dataset):
+    def __init__(self, dataset_folder, list_id, seq_len):
+        self.dataset_folder = dataset_folder
+        self.list_id = list_id
+        self.seq_len = seq_len
+    def __len__(self):
+        return len(self.list_id)
+
+    def __getitem__(self, index):
+        id = self.list_id[index]
+        sig_filename = os.path.join(self.dataset_folder, "sig", "sig_" + id + ".sim")
+        lbl_filename = os.path.join(self.dataset_folder, "lbl", "lbl_" + id + ".sim")
+        sig = sim_sig_read(sig_filename)
+        lbl = sim_lbl_read(lbl_filename)
+
+        X = torch.from_numpy(sig).view(1, self.seq_len)
+        y = torch.from_numpy(lbl).view(1, self.seq_len)
+
+        X, y = X.type(torch.FloatTensor), y.type(torch.FloatTensor)
+
+        return X, y
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embedding_dim, dropout, max_len: int = 300):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, embedding_dim)  # [300, context]
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, embedding_dim, 2).float() * (-math.log(10000.0) / embedding_dim))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(1,2)
+        self.register_buffer('pe', pe)  # allows state-save
+
+    def forward(self, x):
+        #print("x.shape: ", x.shape) # [batch, context, 300]
+        #print("pe.shape: ", self.pe.shape) [1, context, 300]
+        x = x + x + self.pe # [1, context, 300] + [batch, context, 300] = [batch, context, 300] because of broadcasting
+        return self.dropout(x)
+
+class ContextLoader(nn.Module):
+    def __init__(self, embedding_dim):
+        self.embedding_dim = embedding_dim
+        super().__init__()
+
+    def forward(self, x):
+        batch_size, _, seq_len = x.size()
+        x_context = torch.zeros(batch_size, self.embedding_dim, seq_len)
+        for i in range(batch_size):
+            for j in range(seq_len):
+                for k in range(self.embedding_dim):
+                    if j - k < 0:
+                        x_context[i][k][j] = 0
+                    x_context[i][k][j] = x[i][0][j-k]
+        return x_context
+
+class EncoderTransformer(nn.Module):
+    def __init__(self, input_dim, context_dim, d_model, nhead, num_layers, dim_feedforward):
+        super(EncoderTransformer, self).__init__()
+        self.input_linear = nn.Linear(input_dim, d_model)
+        self.transformer_encoder_layer = nn.TransformerEncoderLayer(
+            d_model, nhead, dim_feedforward)
+        self.transformer_encoder = nn.TransformerEncoder(
+            self.transformer_encoder_layer, num_layers)
+        # Output size is same as input size
+        self.output_linear = nn.Linear(d_model, input_dim)
+        self.context_loader = ContextLoader(embedding_dim=context_dim)
+        self.context_linear = nn.Linear(context_dim, 1)
+        self.positional_encoding = PositionalEncoding(embedding_dim=context_dim, dropout=0.1, max_len=input_dim)
+    def forward(self, x):
+        #print(x.shape) #[batch,1,300]
+        x = self.context_loader(x)
+        #print(x.shape) #[batch, context, 300]
+        x = self.positional_encoding(x)
+        #print(x.shape)  #[batch, context, 300]
+        x = self.input_linear(x)
+        # print(x.shape) #[batch, context, 64]
+        x = self.transformer_encoder(x)
+        # print(x.shape) # [batch, context, 64]
+        x = self.output_linear(x)
+        # print(x.shape) # [batch, context, 300]
+        x = x.transpose(-2, -1) #swap context dimension to the last dimension, because linear operator on the last dimension
+        # print(x.shape) # [batch, 300, context]
+        x = self.context_linear(x)
+        # print(x.shape) # [batch, 300, 1]
+        x = x.transpose(-1, -2) #swap back context dimension
+        # print(x.shape) # [batch,1,300]
+        # print("end of transformer")
+        return x
+
+
+# Initialize model
+model = EncoderTransformer(input_dim=300, context_dim=8, d_model=64, nhead=8, num_layers=6, dim_feedforward=128)
+model.train()  # set the model to training mode
+
+# Define loss function and optimizer
+criterion = nn.L1Loss(reduction='mean')
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+# Generate random training data
+sequence_length = 300
+epochs = 100
+batch_size = 32
+data_folder_path = "/home/proj_wavemoto/dataset/generated_data/v5_ss_1m"
+train_data_hparams = {'batch_size': 32,
+                      'shuffle': True,
+                      'num_workers': 10}
+dataset = SimulatedNoiseDataset(os.path.join(data_folder_path,"trn"), [str(num).zfill(5) for num in range(1000)], sequence_length)
+datagen = DataLoader(dataset, **train_data_hparams)
+
+for epoch in range(epochs):
+    total_loss = 0
+    for input_data, target_data in datagen:
+        # Reset the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        output = model(input_data)
+
+        # Compute the loss
+        loss = criterion(output, target_data)
+
+        # Backward pass
+        loss.backward()
+
+        # Update the weights
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    # Print average loss for this epoch
+    avg_loss = total_loss / len(datagen)
+    print(f'Epoch {epoch+1}, Avg Loss: {avg_loss}')
