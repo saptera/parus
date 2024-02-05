5bd03202d696c0058bb46686bb118365967363d1
si-yang <yusiyang.psi@gmail.com>
2024-02-05 22:38:56 +0100

    Positional prediction filter

--- parus/train/inference.py
+++ parus/train/inference.py
@@ -78,3 +78,32 @@ def inference(model, inference_datagen, filename, pred_save_folder):
         pklz_write(os.path.join(pred_save_folder, pred_filename),
                    {"inp": numpy.concatenate(inp_numpy_lst, axis=0),
                     "prd": numpy.concatenate(pred_numpy_lst, axis=0)})
+
+
+def flt_pos(sig_inp, pos_prd, min_dst=5, th=0.5):
+    """ Filter position prediction for multiple points in a window.
+
+    Args:
+        sig_inp (torch.Tensor): Raw signal inputs
+        pos_prd (torch.Tensor): Predicted position, the same shape as [sig_inp]
+        min_dst (int): Window tolerance for binary detection (default: 5)
+        th (int | float): Threshold to make binary tensor (default: 0.5)
+
+    Returns:
+        torch.Tensor: Filtered position prediction
+    """
+    pos = torch.where(pos_prd > th, 1, 0)
+    for k in range(min_dst, 0, -1):
+        win = pos.unfold(dimension=2, size=min_dst, step=k)
+        accu = torch.sum(win, dim=3)
+        chkw = torch.where(accu > 1)
+        for i in range(chkw[0].nelement()):
+            # Find location
+            init = chkw[2][i] * k
+            stop = init + min_dst
+            loc = torch.argmin(sig_inp[chkw[0][i], chkw[1][i], init:stop])
+            # Set value
+            cast = torch.zeros(min_dst, dtype=torch.int)
+            cast[loc] = 1
+            pos[chkw[0][i], chkw[1][i], init:stop] = cast
+    return pos
