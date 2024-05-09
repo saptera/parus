d31e3b6c5d29b7e44a750319158cc699955b2d91
si-yang <yusiyang.psi@gmail.com>
2024-05-09 23:11:34 +0200

    [FIX] Missing metadata in output file, minor print-out fix

--- parus/fio/fdata.py
+++ parus/fio/fdata.py
@@ -385,8 +385,13 @@ def sim_args_read(sim_fp):
             dc = v.decode('utf-8')
             if dc != 'NULL':
                 arg[k] = dc
-        else:
+        elif v.size == 1:
             arg[k] = v.item()
+        else:
+            if v.dtype.kind == 'S':
+                arg[k] = [i.decode() for i in v]
+            else:
+                arg[k] = v.tolist()
     return arg
 
 
