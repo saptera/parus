f6c82535fee2f4d48799b061901804c66f528141
si-yang <yusiyang.psi@gmail.com>
2024-06-30 05:25:22 +0200

    Peak detection PyTorch version

---
+++ parus/model/post_proc.py
@@ -0,0 +1,47 @@
+import torch
+
+"""Function list:
+peak_det_torch(prd, lag, threshold, influence=0.0): Robust signal peak detection using z-scores, PyTorch version.
+"""
+
+
+def peak_det_torch(prd, lag, threshold, influence=0.0):
+    """ Robust signal peak detection using z-scores, PyTorch version.
+        Inspired from J.P.G. van Brakel [https://stackoverflow.com/a/22640362/6029703]
+
+    Args:
+        prd (torch.Tensor): Input prediction results
+        lag (int): The length of data will be smoothed, larger lags should be included for more stationary data
+        threshold (int | float): Threshold of standard deviations from the moving mean above to classify as peak
+        influence (float): {0 ~ 1} The influence of signals on the algorithm's detection threshold (default: 0.0)
+            - 0: Signals have no influence on the threshold, implicitly assume signal is stationary
+            - 1: Signals have full influence of normal data points
+
+    Returns:
+        torch.Tensor: {int32} Detected peak indices. 1 = positive peak, -1 = negative peak, 0 = no peak
+    """
+    # Initialize operational series
+    det = torch.zeros_like(prd, dtype=torch.int)
+    flt = torch.cat((prd[:, :, :lag].flip(dims=(2,)), prd), dim=2)
+    # Compute sliding window initial values
+    fac = 1 / lag
+    lin = torch.sum(flt[:, :, :lag], dim=2)
+    sqr = torch.sum(flt[:, :, :lag].square(), dim=2)
+
+    for i in range(prd.shape[2]):
+        # Sample tensors
+        slc = prd[:, :, i]
+        fpr = flt[:, :, i + lag - 1]
+        fcr = flt[:, :, i + lag]
+        # Update filter
+        avg = lin * fac
+        std = torch.sqrt(torch.abs(sqr * fac - avg * avg))  # abs() to avoid negative value caused by precision loss
+        # Peak detection with influence
+        chk = torch.abs(slc - avg) > threshold * std
+        sgn = torch.where(slc > avg, torch.tensor(1, dtype=torch.int), torch.tensor(-1, dtype=torch.int))
+        det[:, :, i] = sgn * chk.int()
+        flt[:, :, i + lag] = torch.where(chk, influence * slc + (1 - influence) * fpr, fcr)
+        # Update sliding window sums
+        lin = lin + flt[:, :, i + lag] - flt[:, :, i]
+        sqr = sqr + (flt[:, :, i + lag] + flt[:, :, i]) * (flt[:, :, i + lag] - flt[:, :, i])
+    return det
