5a6bc1d81f1e8b167fd92f338e9139c26ef73cd4
si-yang <yusiyang.psi@gmail.com>
2024-10-08 20:29:45 +0200

    HDF5 file reading support

--- parus/fio/__init__.py
+++ parus/fio/__init__.py
@@ -1,4 +1,5 @@
 from .fmeta import *
 from .fdata import *
+from .hdf import *
 from .intan import *
 from .tdt import *
---
+++ parus/fio/hdf.py
@@ -0,0 +1,48 @@
+# Hierarchical Data Format (HDF) file IO functions
+
+import h5py as h5
+
+"""Function list:
+    h5_load_dat(pnt): Load HDF5 data.
+    h5_load_ref(ref, pnt): Load HDF5 data by reference.
+"""
+
+
+def h5_load_dat(pnt):
+    """ Load HDF5 data.
+
+    Args:
+        pnt (h5.File | h5.Group | h5.Dataset): HDF5 file/group/dataset pointer
+
+    Returns:
+        Loaded data
+    """
+    # Read single dataset
+    if isinstance(pnt, h5.Dataset):
+        return pnt[()]
+
+    # Read HDF5 file or group
+    rtv = {}  # INIT VAR
+    for k in pnt.keys():
+        if isinstance(pnt[k], h5.Dataset):
+            rtv[k] = pnt[k][()]
+        else:
+            rtv[k] = h5_load_dat(pnt[k])
+    return rtv
+
+
+def h5_load_ref(ref, pnt):
+    """ Load HDF5 data by reference.
+
+    Args:
+        ref (h5.Reference | h5.RegionReference): HDF5 object reference
+        pnt (h5.File | h5.Dataset): HDF5 file/dataset pointer
+
+    Returns:
+        Loaded data
+    """
+    if isinstance(ref, h5.RegionReference):
+        return pnt[ref]
+    else:
+        tgt = pnt[ref]
+        return h5_load_dat(tgt)
