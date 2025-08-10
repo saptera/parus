15684069bd09493e6b131f1ab64d749eaa2c8d2e
si-yang <yusiyang.psi@gmail.com>
2025-08-10 04:08:23 +0200

    Imporve eval functions for better performance

--- parus/train/eval.py
+++ parus/train/eval.py
@@ -7,63 +7,53 @@ import os
 import numpy
 
 def training_validation(model, datagen, criterion, device):
-    model.to(device)
-    model.eval()
-    with torch.no_grad():
-        val_losses = []
-        for i, (inputs, labels, _) in enumerate(datagen):
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            cur_val_loss = criterion(outputs, labels.float())
-            val_losses.append(cur_val_loss.item())
+    val_losses = []
+    for i, (inputs, labels, _) in enumerate(datagen):
+        inputs, labels = inputs.to(device), labels.to(device)
+        outputs = model(inputs)
+        cur_val_loss = criterion(outputs, labels.float())
+        val_losses.append(cur_val_loss.item())
 
-            # print visual prediction result of the first sample
-            if i == 0:
-                inp_print = inputs.cpu().clone().detach().numpy()[0][0]
-                out_print = outputs.cpu().clone().detach().numpy()[0][0]
-                lab_print = labels.cpu().clone().detach().numpy()[0][0]
-                plt_mdl_perf(out_print, inp_print, lab_print, size=(256, 32))
+        # print visual prediction result of the first sample
+        if i == 0:
+            inp_print = inputs.cpu().clone().detach().numpy()[0][0]
+            out_print = outputs.cpu().clone().detach().numpy()[0][0]
+            lab_print = labels.cpu().clone().detach().numpy()[0][0]
+            plt_mdl_perf(out_print, inp_print, lab_print, size=(256, 32))
 
     return np.mean(val_losses)
 
 
-def inference(model, datagen, filename, inf_save_folder, device, mode="inf", pos_params={"th": -50, "neg": True, "gap": 20}):
-    if mode not in ["inf", "tst"]:
-        raise ValueError(f"Invalid mode '{mode}'. Must be one of: {'inf', 'tst'}")
-    model.to(device)
-    model.eval()
-    with torch.no_grad():
-        inp_numpy_lst = []  
-        spk_pred_numpy_lst = []
-        pos_pred_numpy_lst = []
-        if mode == "tst":
-            labels_numpy_lst = []
-        for item in datagen:
-            if mode == "inf":
-                inputs = item
-            if mode == "tst":
-                inputs, labels = item
-            inputs = inputs.to(device)
-            spk_outputs = model(inputs)
-            pos_outputs = peak_det_diff(spk_outputs, **pos_params)
+def inference(model, datagen, device, th=-50, neg=True, gap=20, test=False):
+    # Initialize lists
+    inp_numpy_lst = []
+    lbl_numpy_lst = []
+    spk_pred_numpy_lst = []
+    pos_pred_numpy_lst = []
+    # Inference
+    for item in datagen:
+        if test:
+            inputs, labels = item
+            lbl_numpy_lst.append(labels.squeeze().cpu().numpy())
+        else:
+            inputs = item
+        inputs = inputs.to(device)
+        spk_outputs = model(inputs)
+        pos_outputs = peak_det_diff(spk_outputs, th=th, neg=neg, gap=gap)
 
-            inp_numpy_lst.append(inputs.squeeze().cpu().numpy())
-            spk_pred_numpy_lst.append(spk_outputs.squeeze().cpu().numpy())
-            pos_pred_numpy_lst.append(pos_outputs.squeeze().cpu().numpy())
-            if mode == "tst":
-                labels_numpy_lst.append(labels.squeeze().cpu().numpy())
-
-        pred_filename = "pred_" + filename
-        inp_numpy = numpy.concatenate(inp_numpy_lst, axis=0)
-        pred_numpy_dict = {"spk": numpy.concatenate(spk_pred_numpy_lst, axis=0),
-                        "pos": numpy.concatenate(pos_pred_numpy_lst, axis=0)}
-        if mode == "inf":
-            pklz_dct = {"inp": inp_numpy, "prd": pred_numpy_dict}
-        if mode == "tst":
-            labels_numpy = numpy.concatenate(labels_numpy_lst, axis=0)
-            pklz_dct = {"inp": inp_numpy, "prd": pred_numpy_dict, "lbl": labels_numpy}
-
-        pklz_write(os.path.join(inf_save_folder, pred_filename), pklz_dct)
+        inp_numpy_lst.append(inputs.squeeze().cpu().numpy())
+        spk_pred_numpy_lst.append(spk_outputs.squeeze().cpu().numpy())
+        pos_pred_numpy_lst.append(pos_outputs.squeeze().cpu().numpy())
+    # Arrange outputs
+    inp_numpy = numpy.concatenate(inp_numpy_lst, axis=0)
+    pred_numpy_dict = {"spk": numpy.concatenate(spk_pred_numpy_lst, axis=0),
+                       "pos": numpy.concatenate(pos_pred_numpy_lst, axis=0)}
+    if test:
+        lbl_numpy = numpy.concatenate(lbl_numpy_lst, axis=0)
+        pklz_dct = {"inp": inp_numpy, "prd": pred_numpy_dict, "lbl": lbl_numpy}
+    else:
+        pklz_dct = {"inp": inp_numpy, "prd": pred_numpy_dict}
+    return pklz_dct
 
 
 def eval_bin_cls(prediction, reference, allowed_distance=0, binary_threshold=0.5):
