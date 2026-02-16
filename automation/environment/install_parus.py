cc4f27ef97ca2b257fda36ca68d793c66b2757a8
si-yang <yusiyang.psi@gmail.com>
2026-02-16 01:08:37 +0100

    Improve installation script, create venv automatically

--- automation/environment/install_parus.py
+++ automation/environment/install_parus.py
@@ -11,34 +11,42 @@ if sys.version_info < (3, 10, 0):
     sys.stderr.write("ERROR: Python 3.10 or higher is required.\n")
     sys.exit(-1)
 
+# Check or build virtual environment
+venv = os.path.join(os.path.abspath(os.path.join(__file__ , '../../..')), 'venv')
+itpr = os.path.join(venv, 'Scripts', 'python.exe')
+if sys.executable != itpr:
+    print("Creating virtual environment...")
+    subprocess.check_call(sys.executable + ' -m venv ' + venv)
+    print("Virtual environment successfully created!\n")
+
 # Upgrade [pip]
 print("Upgrading [pip]")
-subprocess.check_call(sys.executable + ' -m pip install --upgrade pip')
+subprocess.check_call(itpr + ' -m pip install --upgrade pip')
 # Upgrade [setuptools]
 print("\nUpgrading [setuptools]")
-subprocess.check_call(sys.executable + ' -m pip install --upgrade setuptools')
+subprocess.check_call(itpr + ' -m pip install --upgrade setuptools')
 
 # Install [NumPy]
 print("\nInstalling [NumPy]")
-subprocess.check_call(sys.executable + ' -m pip install "numpy>=2.0.0"')
+subprocess.check_call(itpr + ' -m pip install "numpy>=2.0.0"')
 # Install [SciPy]
 print("\nInstalling [SciPy]")
-subprocess.check_call(sys.executable + ' -m pip install "scipy>=1.14.0"')
+subprocess.check_call(itpr + ' -m pip install "scipy>=1.14.0"')
 # Install [h5py] for HDF5 files
 print("\nInstalling [h5py]")
-subprocess.check_call(sys.executable + ' -m pip install h5py')
+subprocess.check_call(itpr + ' -m pip install h5py')
 # Install [matplotlib] for general plotting
 print("\nInstalling [matplotlib]")
-subprocess.check_call(sys.executable + ' -m pip install "matplotlib>=3.8.4"')
+subprocess.check_call(itpr + ' -m pip install "matplotlib>=3.8.4"')
 # Install [plotext] for CLI plotting
 print("\nInstalling [plotext]")
-subprocess.check_call(sys.executable + ' -m pip install plotext')
+subprocess.check_call(itpr + ' -m pip install plotext')
 # Install [PySide6] for GUI
 print("\nInstalling [PySide6]")
-subprocess.check_call(sys.executable + ' -m pip install "PySide6>=6.8"')
+subprocess.check_call(itpr + ' -m pip install "PySide6>=6.8"')
 # Install [PyQtGraph] for real-time plotting
 print("\nInstalling [PyQtGraph]")
-subprocess.check_call(sys.executable + ' -m pip install pyqtgraph')
+subprocess.check_call(itpr + ' -m pip install pyqtgraph')
 # Install [PyTorch]
 print("\nInstalling [PyTorch]")
 try:
@@ -47,19 +55,17 @@ try:
             print("CUDA version: " + line.split()[-1][1:])
             release = ''.join(line.split()[-2][:-1].split('.'))
             command = ' -m pip install torch torchvision --index-url https://download.pytorch.org/whl/cu%s' % release
-            subprocess.check_call(sys.executable + command)
+            subprocess.check_call(itpr + command)
             break
 except FileNotFoundError:
     print("CUDA not found, installing PyTorch CPU")
     command = " -m pip install torch torchvision"
-    subprocess.check_call(sys.executable + command)
+    subprocess.check_call(itpr + command)
 
 # Add project path
 print("\nAdding [Parus] project path to library")
 pth_src = os.path.join(os.path.split(__file__)[0], 'proj_path.pth')
-pth_dst = os.path.join(
-    sys.executable.split('Scripts')[0] if 'Scripts' in sys.executable else os.path.dirname(sys.executable),
-    "Lib/site-packages/proj_path.pth")
+pth_dst = os.path.join(venv, "Lib/site-packages/proj_path.pth")
 shutil.copy2(pth_src, pth_dst)
 
 # Finalize
